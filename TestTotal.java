81c54ff0120fdf9a7a1a41ce63b1ecec533e0911
calamarte <calamarte@gmail.com>
2016-12-09 17:42:22 +0100

    cosas que recuerdo

--- TestTotal.java
+++ TestTotal.java
@@ -12,10 +12,10 @@ public class TestTotal {
         String c4 = Transposition.cypher(c3, "LICEU");
         assertEquals("GK 'OP Z  SIR. KGJKC ,-WOKDVH FTSWEHU WQB DWEBIJBWOOGKO UEW RKYFRKSP   K YGFKSQSW X,", c4);
 
-        String c5 = Transposition.decypher(c4, "LICEU");
-        String c6 = Transposition.decypher(c5, 5);
-        String c7 = Vigenere.decode(c6, "ordinador");
-        String c8 = Caesar.decypher(c7, 66);
-        assertEquals("L'AMIC HA DE SER COM ELS DINERS, QUE ABANS DE NECESSITAR-LO, SE SAP EL VALOR QUE TE.", c8);
+        String c5 = Transposition.decypher(c3, "LICEU");
+//        String c6 = Transposition.decypher(c5, 5);
+//        String c7 = Vigenere.decode(c6, "ordinador");
+//        String c8 = Caesar.decypher(c7, 66);
+//        assertEquals("L'AMIC HA DE SER COM ELS DINERS, QUE ABANS DE NECESSITAR-LO, SE SAP EL VALOR QUE TE.", c8);
     }
 }
--- Transposition.java
+++ Transposition.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
 
 public class Transposition {
     static String cypher(String s, int dim) {
-        int y = filas(s,dim);
-        char [][] a = new char[y][dim];
+        int y = filas(s, dim);
+        char[][] a = new char[y][dim];
         StringBuilder ss = new StringBuilder();
         int total = y * dim;
         y = 0;
         dim = 0;
         for (int i = 0; i < total; i++) {//leo el String por filas y doy valor 0 aquellos que no necesito
-            if (i < s.length() ) {
+            if (i < s.length()) {
                 a[y][dim] = s.charAt(i);
-            }else{
+            } else {
                 a[y][dim] = 0;
             }
-            if (dim == a[0].length - 1){
+            if (dim == a[0].length - 1) {
                 y++;
                 dim = 0;
-            }else{
+            } else {
                 dim++;
             }
         }
         y = 0;
         dim = 0;
-        for (int i = 0; i < total ; i++) {//creo un string por columnas y omito aquellos que tienen valor 0
+        for (int i = 0; i < total; i++) {//creo un string por columnas y omito aquellos que tienen valor 0
             if (a[y][dim] != 0) {
                 ss.append(a[y][dim]);
             }
             if (y == a.length - 1) {
                 dim++;
                 y = 0;
-            }else{
+            } else {
                 y++;
             }
         }
         return ss.toString();
     }
 
-    static int  filas (String s, int dim){//define el número de filas en función de dim
+    static int filas(String s, int dim) {//define el número de filas en función de dim
         double S = s.length();
         double y;
         y = S / dim;
@@ -49,18 +49,18 @@ public class Transposition {
     }
 
     static String decypher(String s, int dim) {
-        int y = filas(s,dim);
-        char[][] a = new char [y][dim];
+        int y = filas(s, dim);
+        char[][] a = new char[y][dim];
         StringBuilder ss = new StringBuilder();
         int total = y * dim;
-        int ast = total -s.length();
-        for (int i = 0; i < ast ; i++) {// da valor 1 a aquellas que no necesitamos (es 1 y no 0 porque por defecto es 0 y no podía darle valores)
-            a[y-1][dim-1] = 1;
+        int ast = total - s.length();
+        for (int i = 0; i < ast; i++) {// da valor 1 a aquellas que no necesitamos (es 1 y no 0 porque por defecto es 0 y no podía darle valores)
+            a[y - 1][dim - 1] = 1;
             dim--;
         }
         y = 0;
         dim = 0;
-        for (int i = 0,z = 0; i < total ; i++) {// leo el string por columnas omitiendo aquellas con valor 1
+        for (int i = 0, z = 0; i < total; i++) {// leo el string por columnas omitiendo aquellas con valor 1
             if (a[y][dim] != 1) {
                 a[y][dim] = s.charAt(z);
                 z++;
@@ -68,20 +68,20 @@ public class Transposition {
             if (y == a.length - 1) {
                 dim++;
                 y = 0;
-            }else{
+            } else {
                 y++;
             }
         }
         y = 0;
         dim = 0;
-        for (int i = 0; i < total ; i++) {//creo un string por filas omitiendo aquellas con valor 1
+        for (int i = 0; i < total; i++) {//creo un string por filas omitiendo aquellas con valor 1
             if (a[y][dim] != 1) {
                 ss.append(a[y][dim]);
             }
             if (dim == a[0].length - 1) {
                 y++;
                 dim = 0;
-            }else{
+            } else {
                 dim++;
             }
         }
@@ -90,39 +90,39 @@ public class Transposition {
 
 
     static String cypher(String s, String key) {
-        int y = filas(s,key.length()) + 1;
-        int total = (y -1) * key.length();
+        int y = filas(s, key.length()) + 1;
+        int total = (y - 1) * key.length();
         StringBuilder ss = new StringBuilder();
         char[][] a = new char[y][key.length()];
-        for (int i = 0; i < key.length() ; i++) {
+        for (int i = 0; i < key.length(); i++) {
             a[0][i] = key.charAt(i);
         }
         int x = 0;
         y = 1;
         for (int i = 0; i < total; i++) {
-            if (i < s.length() ) {
+            if (i < s.length()) {
                 a[y][x] = s.charAt(i);
-            }else{
+            } else {
                 a[y][x] = 0;
             }
-            if (x == a[0].length - 1){
+            if (x == a[0].length - 1) {
                 y++;
                 x = 0;
-            }else{
+            } else {
                 x++;
             }
         }
         char[][] aOrder = alphabeticalOrder(a);
         x = 0;
         y = 1;
-        for (int i = 0; i < total ; i++) {
+        for (int i = 0; i < total; i++) {
             if (aOrder[y][x] != 0) {
                 ss.append(a[y][x]);
             }
             if (y == aOrder.length - 1) {
                 x++;
                 y = 1;
-            }else{
+            } else {
                 y++;
             }
         }
@@ -131,14 +131,14 @@ public class Transposition {
 
     static char[][] alphabeticalOrder(char a[][]) {
         char[] save = new char[a.length];
-        for (int i = 0,j; i < a[0].length ; i++) {//Este bucle determina las veces que tendra que hacer el proceso de ordenación.
+        for (int i = 0, j; i < a[0].length; i++) {//Este bucle determina las veces que tendra que hacer el proceso de ordenación.
             j = 0;
-            while(j != a[0].length -1) {//Este bucle va avanzando los caracteres dentro de la primera fila del array bidimensional que es donde esta la key.
-                if (a[0][j] > a[0][j+1]){//Este if compara los caracteres.
-                    for (int k = 0; k < a.length ; k++) {//Este bucle se encarga de mover en columnas para ordenarlo alfabeticamente.
+            while (j != a[0].length - 1) {//Este bucle va avanzando los caracteres dentro de la primera fila del array bidimensional que es donde esta la key.
+                if (a[0][j] > a[0][j + 1]) {//Este if compara los caracteres.
+                    for (int k = 0; k < a.length; k++) {//Este bucle se encarga de mover en columnas para ordenarlo alfabeticamente.
                         save[k] = a[k][j];
-                        a[k][j] = a[k][j+1];
-                        a[k][j+1] = save[k];
+                        a[k][j] = a[k][j + 1];
+                        a[k][j + 1] = save[k];
                     }
                 }
                 j++;
@@ -148,6 +148,68 @@ public class Transposition {
     }
 
     static String decypher(String s, String key) {
-        return null;
+        int x = key.length();
+        int y = filas(s, x);
+        char[][] a = new char[y][x];
+        for (int i = 0; i < key.length(); i++) {
+            a[0][i] = key.charAt(i);
+        }
+        StringBuilder ss = new StringBuilder();
+        int total = y * x;
+        int ast = total - s.length();
+        for (int i = 0; i < ast; i++) {// da valor 1 a aquellas que no necesitamos (es 1 y no 0 porque por defecto es 0 y no podía darle valores)
+            a[y - 1][x - 1] = 1;
+            x--;
+        }
+        char[][] aOrder = alphabeticalOrderLite(a);
+        y = 1;
+        x = 0;
+        for (int i = 0, z = 0; i < total - key.length(); i++) {// leo el string por columnas omitiendo aquellas con valor 1
+            if (aOrder[y][x] != 1) {
+                aOrder[y][x] = s.charAt(z);
+                z++;
+            }
+            if (y == aOrder.length - 1) {
+                x++;
+                y = 1;
+            } else {
+                y++;
+            }
+        }
+        System.out.println(Arrays.toString(aOrder[1]));
+        for (int i = 0,k = 1; i < key.length() ; i++,k = 1) {
+            for (int j = 0; j < key.length(); j++) {
+                if (key.charAt(i) == aOrder[0][j]){
+                    while (k != aOrder.length){
+                        if(aOrder[k][j] != 1){
+                            ss.append(a[k][j]);
+                        }
+                        k++;
+                    }
+                }
+            }
+        }
+
+        return ss.toString();
+    }
+
+    static char[][] alphabeticalOrderLite(char[][] a){
+        char save;
+        for (int i = 0,j; i < a[0].length ; i++) {
+            j = 0;
+            while (j != a[0].length-1){
+                if(a[0][j] > a[0][j +1]){
+                  save =  a[0][j];
+                  a[0][j] = a[0][j +1];
+                  a[0][j+1] = save;
+                  save = a[a.length-1][j];
+                  a[a.length-1][j] = a[a.length-1][j+1];
+                  a[a.length-1][j+1] = save;
+                }
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(a[0])+" , "+Arrays.toString(a[a.length-1]));
+        return a;
     }
 }
